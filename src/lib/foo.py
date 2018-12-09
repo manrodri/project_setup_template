10fec9f3febbfca0df57664d073194ad480e01ad
manrodri <32561150+manrodri@users.noreply.github.com>
2018-12-09 17:27:44 +0000

    Update foo.py

--- src/lib/foo.py
+++ src/lib/foo.py
@@ -1,14 +1,2 @@
-import os
-
-CURRENT_DIR = os.getcwd()
-ROOT_DIR = os.path.abspath(os.path.join(CURRENT_DIR, '..','..'))
-
-def get_test_files():
-    for root, dirs, files in os.walk(ROOT_DIR):
-        print (files)
-
-
-
-get_test_files()
-
-
+class foo:
+    pass
